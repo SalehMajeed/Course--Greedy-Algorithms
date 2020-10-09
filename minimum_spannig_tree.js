d02d7346a30d728911b3499c03ad25847c765d35
= <saleh.majeed8@gmail.com>
2020-10-09 14:10:20 +0530

    mst

---
+++ minimum_spannig_tree.js
@@ -0,0 +1,9 @@
+/*
+    1. doesn't containt cycle.
+    2. minimum edges weight.
+    3. undirected graph.
+    4. Applications ->
+        I - Network Design.
+        II - Approximation Algorithm.
+        III -> Cluster Analysis.
+*/
