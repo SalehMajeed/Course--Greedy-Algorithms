15ec8718262981ebd6604db7e0863e4d007a4eb3
= <saleh.majeed8@gmail.com>
2020-10-09 13:34:45 +0530

    table_contents

---
+++ table_contents.js
@@ -0,0 +1,11 @@
+/*
+    1. Introduction To Greedy Algorithm.
+    2. Activity Selection Problem.
+    3. Minimum Spanning Tree.
+    4. Dijkstra's Shortest path Algorithm.
+    5. Job Sequencing.
+    6. Krushkal's Algorithm.
+    7. Knapsack Algorithm.
+    8. Huffman Coding.
+    9. Prim's Algorithm.
+*/
